4b8c75f9bc9bdf82aab3d28c773468c4bff8eeb2
MusicGindos <rlgindos@gmail.com>
2017-01-06 17:36:08 +0200

    deleted unnecessary views

--- manage_urls_app/views.py
+++ manage_urls_app/views.py
@@ -1,15 +1,10 @@
 from django.http import HttpResponse
 from manage_urls_app import searchCeleb
 from manage_urls_app import celebTweets
-def index(request):
-	return HttpResponse('<h1>Hello World! </h1>')
 
-def item_details(request, id):
-	return HttpResponse('<p1>in item details with id {0} <p1>'.format(id))
 
 def getFamous(request):
 	return HttpResponse(searchCeleb.getFamouses())
 
 def getFamousProfile(request,name):
 	return HttpResponse(celebTweets.searchTweetsForCeleb(name))
-#	#TODO: return name,picture,bad word(array),bad words with count(array) how many said the bad word,how many retweet for page 2
