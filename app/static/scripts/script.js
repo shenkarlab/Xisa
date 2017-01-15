cecd7fa190b94742a6b6b8927a08db11b33a81a2
Gregory <kogrego@gmail.com>
2017-01-15 17:06:17 +0200

    0.1.0

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var app = angular.module('xisa', []);
-
 var path = 'https://xisasimpleserver.herokuapp.com/api/';
+var flag = false;
 
 function getUrlParameter(param) {
   var sPageURL = window.location.search.substring(1),
@@ -18,30 +18,56 @@ function getUrlParameter(param) {
   return res;
 }
 
+function filterBtnClick(filter){
+   if(flag){
+    $('nav ul').css('opacity', '1');
+    $('#leftText').css('opacity', '1');
+    $('#cubeContainer').css('opacity', '1');
+    $('.filterOptions').removeClass('active');
+    $('#close').addClass('hidden');
+    $('.filterItem').slideToggle(200);
+    $('#cubeContainer').empty();
+    $http.get('/api/getCelebs').then(function (response){
+      angular.forEach(response.data, function(value){
+        var cube =  '<a href="/how?name='+value.name+'&word='+value.word+'&url='+value.image+'">'+
+                      '<section class="cube" id="cube'+i+'">'+
+                        '<p id="hashtag"><span class="highlight">'+value.word+'</span></p>'+
+                        '<p id="celebName"><span class="highlight">'+value.name+'</span></p>'+
+                      '</section>'+
+                    '<a>';
+        $('#cubeContainer').append(cube);
+        var url = 'url('+value.image+')';
+        $('#cube'+i).css('background',url);
+        i++;
+      })
+    },function (error){
+      $('#cubeContainer').append('<section class="error">500<br>Twitter connection error</section>');
+    });
+    flag = false;
+  }
+}
+
 app.controller('whoCtrl', function($scope, $http) {
-    $http.get('/api/getCelebs')
-	    .then(function (response){
-        $scope.mostHated = 'MOST HATED';
-        $scope.people = 'PEOPLE';
-        $scope.lowerBuzz = 'lower Buzz';
-        $scope.higherBuzz = 'higher Buzz';
-        $('retweet').append('<img id="bar" src="images/bar.png" alt="color bar">');
-    		var i = 1;
-	    	angular.forEach(response.data, function(value){
-	    		var cube =	'<a href="/how?name='+value.name+'&word='+value.word+'&url='+value.image+'">'+
-      	    						'<section class="cube" id="cube'+i+'">'+
-        									'<p id="hashtag"><span class="highlight">'+value.word+'</span></p>'+
-                          '<p id="celebName"><span class="highlight">'+value.name+'</span></p>'+
-        								'</section>'+
-        							'<a>';
-  				$('#cubeContainer').append(cube);
-          var url = 'url('+value.image+')';
-          $('#cube'+i).css('background',url);
-  				i++;
-	    	})
-      },function (error){
-        $('#cubeContainer').append('<section class="error">500<br>Twitter connection error</section>');
-		});
+    $http.get('/api/getCelebs').then(function (response){
+      $scope.mostHated = 'MOST HATED';
+      $scope.people = 'PEOPLE';
+      $scope.past = 'The past 7 days on Twitter';  
+  		var i = 1;
+    	angular.forEach(response.data, function(value){
+    		var cube =	'<a href="/how?name='+value.name+'&word='+value.word+'&url='+value.image+'">'+
+    	    						'<section class="cube" id="cube'+i+'">'+
+      									'<p id="hashtag"><span class="highlight">'+value.word+'</span></p>'+
+                        '<p id="celebName"><span class="highlight">'+value.name+'</span></p>'+
+      								'</section>'+
+      							'<a>';
+				$('#cubeContainer').append(cube);
+        var url = 'url('+value.image+')';
+        $('#cube'+i).css('background',url);
+				i++;
+    	})
+    },function (error){
+      $('#cubeContainer').append('<section class="error">500<br>Twitter connection error</section>');
+	});
 });
 
 app.controller('howCtrl', function($scope, $http) {
@@ -51,47 +77,67 @@ app.controller('howCtrl', function($scope, $http) {
     lastName = getUrlParameter('name');
    }
    var request = decodeURIComponent('/api/celeb/'+lastName);
-   $http.get(request)
-	    .then(function (response){
-        var url = getUrlParameter('url');
-        var urlAppend = 'url('+url+')';
-        $('body').css('background',urlAppend);
-        $('body').css('background-repeat','no-repeat');
-        $('body').css('background-size','100% 100%');
-	    	var badWord = getUrlParameter('word');
-	    	$scope.celebName = decodeURIComponent(getUrlParameter('name')).toUpperCase();
-	     	$scope.twitterName = decodeURIComponent(getUrlParameter('name'));
-        $scope.numOfPeople = 'This week, People said 100 times That he is ';
-        $scope.badWord = badWord;
-        $scope.also = 'They also said about him:';
-        var i = 0;
-        angular.forEach(response.data, function(data){
-          var barDiv =  '<p id="barBadWord">'+data.word.toUpperCase()+'</p>'+
-                        '<div class="bar_chart" style="width:'+data.bad_words_count*3+'px;"></div>'+
-                        '<p id="barWordCount">'+data.bad_words_count+'</p>'
-          $("#bars").append(barDiv);
-          var texts = "";
-          texts += '<div class="TickerNews" id="text_move'+(i+1)+'"> <div class="ti_wrapper"> <div class="ti_slide"> <div class="ti_content"> ';
-          data.texts.forEach(function(text){
-            texts += '<div class="ti_news">';
-            text.split(" ").forEach(function(word){
-              if(word == data.word){
-                texts += '<span id="markWord">'+word.toUpperCase()+'&nbsp</span>'
-              } else {
-                texts += word.toUpperCase() + '&nbsp';
-              }
-            })
-            texts += '&nbsp&nbsp + &nbsp&nbsp</div> ';
-          });
-          texts += '</div> </div> </div> </div>';
-          $("#bad_words").append(texts);
-          i++;
+   $http.get(request).then(function (response){
+    var url = getUrlParameter('url');
+    var urlAppend = 'url('+url+')';
+    $('body').css('background',urlAppend);
+    $('body').css('background-repeat','no-repeat');
+    $('body').css('background-size','100% 100%');
+  	var badWord = getUrlParameter('word');
+  	$scope.celebName = decodeURIComponent(getUrlParameter('name')).toUpperCase();
+   	$scope.twitterName = '@'+decodeURIComponent(getUrlParameter('name'));
+    $scope.numOfPeople = 'This week, People said 100 times That he is ';
+    $scope.badWord = badWord;
+    $scope.also = 'They also said about him:';
+    var i = 0;
+    angular.forEach(response.data, function(data){
+      var barDiv =  '<p id="barBadWord">'+data.word.toUpperCase()+'</p>'+
+                    '<div class="bar_chart" style="width:'+data.bad_words_count*3+'px;"></div>'+
+                    '<p id="barWordCount">'+data.bad_words_count+' times</p>';
+      $("#bars").append(barDiv);
+      var texts = "";
+      texts += '<div class="TickerNews" id="text_move'+(i+1)+'"> <div class="ti_wrapper"> <div class="ti_slide"> <div class="ti_content"> ';
+      data.texts.forEach(function(text){
+        texts += '<div class="ti_news">';
+        text.split(" ").forEach(function(word){
+          if(word == data.word){
+            texts += '<span id="markWord">'+word.toUpperCase()+'&nbsp</span>'
+          } else if(word.startsWith('@')) {
+            texts += '<span class="whatmark">'+word.toUpperCase()+'&nbsp</span>'
+          } else {
+            texts += word.toUpperCase() + '&nbsp';
+          }
         })
-        $('#howContent').append('<div class="clear"></div>');
-        $("#text_move1,#text_move2,#text_move3,#text_move4,#text_move5").newsTicker();
-	    },function (error){
-          $('#howContent').append('<section class="error">500<br>Twitter connection error</section>');
-		});
+        texts += '&nbsp&nbsp + &nbsp&nbsp</div> ';
+      });
+      texts += '</div> </div> </div> </div>';
+      $("#bad_words").append(texts);
+      var news = $("#text_move"+(i+1)).newsTicker();
+      $("#text_move"+(i+1)).hover(function(){
+       news[0].pauseTicker();
+       for(var l = 0; l < k; l++){
+        var ref = $('#ti_news'+(l+1)+' .whatmark');
+          ref.hover(function(){
+            ref.removeClass('whatmark');
+            ref.addClass('redUnder');
+            ref.click(function(){
+              window.location.href = '/what?name='+ref.html();
+            })
+          }, function(){
+            ref.removeClass('redUnder');
+            ref.addClass('whatmark');
+          });
+         }
+        }, function(){
+        news[0].startTicker();
+      });
+    })
+    i++;
+    $('#howContent').append('<div class="clear"></div>');
+    $("#text_move1,#text_move2,#text_move3,#text_move4,#text_move5").newsTicker();
+  },function (error){
+      $('#howContent').append('<section class="error">500<br>Twitter connection error</section>');
+  });
 });
 
 app.controller('whatCtrl', function($scope, $http) {
@@ -100,82 +146,114 @@ app.controller('whatCtrl', function($scope, $http) {
    if(lastName == null){
     lastName = getUrlParameter('name');
    }
-   var request = decodeURIComponent('/api/celeb/'+lastName);
-   $http.get(request)
-      .then(function (response){
-        var url = getUrlParameter('url');
-        var urlAppend = 'url('+url+')';
-        $('body').css('background',urlAppend);
-        $('body').css('background-repeat','no-repeat');
-        $('body').css('background-size','100% 100%');
-        var badWord = getUrlParameter('word');
-        $scope.firstName = decodeURIComponent(getUrlParameter('name')).toUpperCase().split(" ")[0];
-        $scope.lastName = decodeURIComponent(getUrlParameter('name')).toUpperCase().split(" ")[1];
-        $scope.twitterName = getUrlParameter('name').replace("%20", " ");
-        $scope.followersCount = '3000 followers';
-        $scope.numOfPeople = 'This week, '+$scope.firstName+' said ';
-        $scope.badWordCount = 345;
-        $scope.picText = "She doesn't like these people: "
-        $scope.also = ' offencive words';
-        var i = 0;
-        for (var j = 0; j < 5; j++){
-          var pic = '<img src="'+url+'" alt="hatedPic" class="pics">';
-          $('#pics').append(pic);
-        }
-        $('#pics').append('<div class="clear"></div>');
-        angular.forEach(response.data, function(data){
-          var barDiv =  '<p id="whatbarBadWord">'+data.word.toUpperCase()+'</p>'+
-                        '<div class="whatbar_chart" style="width:'+data.bad_words_count*3+'px;"></div>'+
-                        '<p id="whatbarWordCount">'+data.bad_words_count+'</p>'
-          $("#whatbars").append(barDiv);
-          var texts = "";
-          texts += '<div class="TickerNews" id="text_move'+(i+1)+'"> <div class="ti_wrapper"> <div class="ti_slide"> <div class="ti_content"> ';
-          data.texts.forEach(function(text){
-            texts += '<div class="ti_news">';
-            text.split(" ").forEach(function(word){
-              if(word == data.word){
-                texts += '<span id="whatmarkWord">'+word.toUpperCase()+'&nbsp</span>'
-              } else {
-                texts += word.toUpperCase() + '&nbsp';
-              }
-            })
-            texts += '&nbsp&nbsp + &nbsp&nbsp</div> ';
-          });
-          texts += '</div> </div> </div> </div>';
-          $("#whatbad_words").append(texts);
-          i++;
+   var request = decodeURIComponent('/api/user/'+lastName);
+   $http.get(request).then(function (response){
+    var url = response.data.user_details.image;
+    var urlAppend = 'url('+url+')';
+    $('body').css('background',urlAppend);
+    $('body').css('background-repeat','no-repeat');
+    $('body').css('background-size','100% 100%');
+    console.log(response.data);
+    $scope.firstName = response.data.user_details.name.split(" ")[0].toUpperCase();
+    $scope.lastName = ($scope.firstName.length > 1) ? response.data.user_details.name.split(" ")[1].toUpperCase() : '';
+    $scope.twitterName = '@'+response.data.user_details.screen_name;
+    $scope.followersCount = response.data.user_details.followers_count + ' followers';
+    $scope.numOfPeople = 'This week, '+$scope.twitterName+' said ';
+    $scope.picText = "She doesn't like these people: "
+    $scope.also = ' offensive words';
+    var i = 0;
+    var badWordCount = 0;
+    for (var j = 0; j < 5; j++){
+      var pic = '';
+      pic += '<section id="redDimmer" class="pics" style="background: url('+url+'); background-size: contain;">';
+      pic += '</section>';
+      $('#pics').append(pic);
+    }
+    $('#pics').append('<div class="clear"></div>');
+    angular.forEach(response.data.words_with_texts, function(data){
+      badWordCount += data.count;
+      var barDiv =  '<p id="whatbarBadWord">'+data.word.toUpperCase()+'</p>'+
+                    '<div class="whatbar_chart" style="width:'+data.count*3+'px;"></div>'+
+                    '<p id="whatbarWordCount">'+data.count+' times</p>';
+      $("#whatbars").append(barDiv);
+      var texts = "";
+      texts += '<div class="TickerNews" id="text_move'+(i+1)+'"> <div class="ti_wrapper"> <div class="ti_slide"> <div class="ti_content"> ';
+      var k = 0;
+      data.texts.forEach(function(text){
+        texts += '<div class="ti_news" id="ti_news'+k+'">';
+        text.tweet.split(" ").forEach(function(word){
+          if(word == data.word){
+            texts += '<span id="whatmarkWord">'+word.toUpperCase()+'&nbsp</span>'
+          } else if(word.startsWith('@')) {
+            texts += '<span class="whatmark">'+word.toUpperCase()+'&nbsp</span>'
+          } else {
+            texts += word.toUpperCase() + '&nbsp';
+          }
         })
-        $('#whatContent').append('<div class="clear"></div>');
-        $("#text_move1,#text_move2,#text_move3,#text_move4,#text_move5").newsTicker();
-      },function (error){
-          $('#whatContent').append('<section class="error">500<br>Twitter connection error</section>');
+        texts += '&nbsp&nbsp + &nbsp&nbsp</div> ';
+        k++;
+      });
+      texts += '</div> </div> </div> </div>';
+      $("#whatbad_words").append(texts);
+      var news = $("#text_move"+(i+1)).newsTicker();
+      $("#text_move"+(i+1)).hover(function(){
+       news[0].pauseTicker();
+       for(var l = 0; l < k; l++){
+        var ref = $('#ti_news'+(l+1)+' .whatmark');
+          ref.hover(function(){
+            ref.removeClass('whatmark');
+            ref.addClass('redUnder');
+            ref.click(function(){
+              window.location.href = '/what?name='+ref.html();
+            })
+          }, function(){
+            ref.removeClass('redUnder');
+            ref.addClass('whatmark');
+          });
+         }
+        }, function(){
+        news[0].startTicker();
+      });
+      i++;
     });
+    $scope.badWordCount = badWordCount;
+    $('#whatContent').append('<div class="clear"></div>');
+    },function (error){
+      $('#whatContent').append('<section class="error">500<br>Twitter connection error</section>');
+  });
 });
 
 app.controller('whomCtrl', function($scope, $http) {
-    $http.get('/api/getCelebs')
-      .then(function (response){
-        $scope.mostOffencive = 'MOST OFFENCIVE';
-        $scope.users = 'USERS';
-        $scope.lowerFollowers = 'lower followers';
-        $scope.higherFollowers = 'higher followers';
-        $('whomretweet').append('<img id="bar" src="images/redbar.png" alt="color bar">');
-        var i = 1;
-        angular.forEach(response.data, function(value){
-          var cube =  '<a href="/what?name='+value.name+'&word='+value.word+'&url='+value.image+'">'+
-                          '<section class="whomcube" id="whomcube'+i+'">'+
-                          '<p id="userName"><span class="blackHighlight">'+value.word+'</span></p>'+
-                          '<p id="location"><span class="blackHighlight">'+value.name+'</span></p>'+
-                        '</section>'+
-                      '<a>';
-          $('#whomcubeContainer').append(cube);
-          var url = 'url('+value.image+')';
-          $('#whomcube'+i).css('background',url);
-          i++;
-        })
-      },function (error){
-        $('#whomcubeContainer').append('<section class="error">500<br>Twitter connection error</section>');
-    });
+    $http.get('/api/getUsers').then(function (response){
+    $scope.mostHating = 'MOST HATING';
+    $scope.users = 'USERS';
+    $scope.past = 'The past 7 days on Twitter';
+    var i = 1;
+    angular.forEach(response.data, function(value){
+      var names = value.tweeter_name.split(" ");
+      var name = "";
+      angular.forEach(names, function(val){
+        if(names.length <= 1){
+          name = names[0] + '<br>';
+        }
+        else{
+          name += val + '<br>';
+        }
+      });
+      var followersCount = '<span id="location"><span id="background">'+value.followers_count+' followers</span></span>';
+      var cube =  '<a href="/what?name='+value.tweeter_name+'">'+
+                      '<section class="cube" id="cube'+i+'">'+
+                      '<p id="userName"><span class="blackHighlight">'+name+'</span>'+followersCount+'</p>'+
+                    '</section>'+
+                  '<a>';
+      $('#cubeContainer').append(cube);
+      var url = 'url('+value.image+')';
+      $('#cube'+i).css('background',url);
+      i++;
+    })
+  },function (error){
+    $('#cubeContainer').append('<section class="error">500<br>Twitter connection error</section>');
+  });
 });
 
 app.controller('navCtrl', ['$scope', '$location', function ($scope, $location) {
@@ -183,25 +261,62 @@ app.controller('navCtrl', ['$scope', '$location', function ($scope, $location) {
     $scope.navLinks = [{
         Title: '/',
         Alias: '/',
-        LinkText: 'WHO'
-    }, {
-        Title: '#',
-        Alias: 'how',
-        LinkText: 'HOW'
-    }, {
-        Title: '#',
-        Alias: 'what',
-        LinkText: 'WHAT'
+        LinkText: 'HATED'
     }, {
         Title: '/whom',
         Alias: 'whom',
-        LinkText: 'WHOM'
+        LinkText: 'HATER'
     }];
 
     $scope.navClass = function (page) {
         var currentRoute = window.location.pathname.substring(1);
         currentRoute = currentRoute.split("?")[0] || '/';
+        if(currentRoute.startsWith('what')){
+          currentRoute = 'whom';
+        } else if(currentRoute.startsWith('how')){
+          currentRoute = '/';
+        }
         return page === currentRoute ? 'active' : '';
     };   
 }]);
 
+app.controller('filterCtrl', ['$scope', function ($scope, $location) {
+
+    $scope.filterButtons = [{
+      Name: 'BODY',
+      Id: 'filterBody',  
+    },{
+      Name: 'GENDER',
+      Id: 'filterGender', 
+    },{
+      Name: 'VICTIM',
+      Id: 'filterVictim',
+    }];
+  
+  $('.filterOptions').click(function() {
+    if(!flag){
+      $('.filterOptions').addClass('active');
+      $('#close').removeClass('hidden');
+      $('.filterItem').slideToggle(200);
+      $('nav ul').css('opacity', '0.1');
+      $('#leftText').css('opacity', '0.1');
+      $('#cubeContainer').css('opacity', '0.1');
+      $("#filterBody").click(filterBtnClick('body'));
+      $("#filterGender").click(filterBtnClick('gender'));
+      $("#filterVictim").click(filterBtnClick('victim'));
+      flag = true;
+    }
+  });
+
+  $('#close').click(function(){
+    if(flag){
+      $('nav ul').css('opacity', '1');
+      $('#leftText').css('opacity', '1');
+      $('#cubeContainer').css('opacity', '1');
+      $('.filterOptions').removeClass('active');
+      $('#close').addClass('hidden');
+      $('.filterItem').slideToggle(200);
+      flag = false;
+    }
+  });
+}]);
--- index.js
+++ index.js
@@ -4,13 +4,12 @@ var express	= require('express'),
 	cors 	= require('cors'),
 	request = require('request');
 
-const http = require('http');
-
 var port 			= process.env.PORT || 3000,
 	app 			= express(),
 	clientRootPath 	= __dirname + '/app/',
 	apiPath 		= 'xisaserver.herokuapp.com',
-	clientPort 			= 8080;
+	clientPort 		= 8080,
+	DEBUG			= true;	
 
 //server config
 app.use(express.static(__dirname + '/app/static'));
@@ -27,10 +26,6 @@ app.use(function(req, res, next) {
 //routes
 
 app.all('/api/*', (req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, content-type, Accept');
-    res.setHeader('Access-Control-Allow-Credentials', true);
 	res.setHeader("Content-Type", "application/json");
 	next()
 });
@@ -76,32 +71,95 @@ app.get('/whom', (req,res,next) => {
 });
 
 app.get('/api/getCelebs', (req,res,next) => {
-	request('http://localhost:8080/getCelebs', function (error, response, body) {
-	  if(error){
-	  	console.log(error);
-	  }
-	  if (!error && response.statusCode == 200) {
-	    console.log(body);
-	    return res.status(200).json(JSON.parse(body));
-	  }
-	})
+	if(DEBUG){
+		fs.readFile('D:/Shenkar/XISA/source/who.json', (err, json) => {
+			if (err) {
+	        	throw err; 
+	    	}
+	    	res.json(JSON.parse(json)); 
+		});
+	}
+	else {
+		request('http://localhost:8080/getCelebs', (error, response, body) => {
+			if(error){
+				console.log(error);
+			}
+			if (!error && response.statusCode == 200) {
+			console.log(body);
+			return res.status(200).json(JSON.parse(body));
+			}
+		});
+	}
 });
 
 app.get('/api/celeb/:name', (req,res,next) => {
-	var queryRequest = 'http://localhost:8080/celeb/'+req.params.name;
-	request(queryRequest, function (error, response, body) {
-	  if(error){
-	  	console.log(error);
-	  }
-	  if (!error && response.statusCode == 200) {
-	    console.log(body);
-	    return res.status(200).json(JSON.parse(body));
-	  }
-	})
+	if(DEBUG){
+		fs.readFile('D:/Shenkar/XISA/source/how.json', (err, json) => {
+			if (err) {
+	        	throw err; 
+	    	}
+	    	res.json(JSON.parse(json)); 
+		});
+	}
+	else {
+		var queryRequest = 'http://localhost:8080/celeb/'+req.params.name;
+		request(queryRequest,  (error, response, body) => {
+			if(error){
+			console.log(error);
+			}
+			if (!error && response.statusCode == 200) {
+			return res.status(200).json(JSON.parse(body));
+			}
+		});
+	}
+});
+
+app.get('/api/getUsers', (req,res,next) => {
+	if(DEBUG){
+		fs.readFile('D:/Shenkar/XISA/source/users.json', (err, json) => {
+			if (err) {
+	        	throw err; 
+	    	}
+	    	res.json(JSON.parse(json)); 
+		});
+	}
+	else {
+		var queryRequest = null; //TODO: 'http://localhost:8080/celeb/'+req.params.name;
+		request(queryRequest,  (error, response, body) => {
+			if(error){
+				console.log(error);
+			}
+			if (!error && response.statusCode == 200) {
+			return res.status(200).json(JSON.parse(body));
+			}
+		});
+	}
+});
+
+app.get('/api/user/:name', (req,res,next) => {
+	if(DEBUG){
+		fs.readFile('D:/Shenkar/XISA/source/user.json', (err, json) => {
+			if (err) {
+	        	throw err; 
+	    	}
+	    	res.json(JSON.parse(json)); 
+		});
+	}
+	else {
+		var queryRequest = null; //TODO: 'http://localhost:8080/celeb/'+req.params.name;
+		request(queryRequest,  (error, response, body) => {
+			if(error){
+				console.log(error);
+			}
+			if (!error && response.statusCode == 200) {
+			return res.status(200).json(JSON.parse(body));
+			}
+		});
+	}
 });
 
 app.get('*', (req,res) => {
-	res.end('404');
+	res.status(404).end('404');
 });
 
 //create server
