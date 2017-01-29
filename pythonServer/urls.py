ce641dcfcdb2858ab8fa1f5c58a7bdd9243835c2
Dassi <DassiShenkar@users.noreply.github.com>
2017-01-29 04:06:47 +0200

    Delete urls.py

--- pythonServer/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from manage_urls_app import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^getCelebs/', views.get_celebs),
-    url(r'^celeb/(?P<name>[\w\-]+)/$', views.get_celeb_profile, name='Celeb Name')
-]
