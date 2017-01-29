dbede9fff10e1df32a1973982363ac12e624bfbb
Dassi <DassiShenkar@users.noreply.github.com>
2017-01-29 04:04:03 +0200

    Delete config.py

--- manage_urls_app/config.py
+++
@@ -1,4 +0,0 @@
-consumer_key = "WJCuAK9k41Ks1L6MmAgroIoOC"
-consumer_secret = "HuNvhLU8QT3z9jjP38lp99pEfrxjOR1mqoRdTF2sTICjCJP5dN"
-access_key = "809132041363222528-SywcqlS0wHqQYHtC8lkm482RgYMg0T4"
-access_secret = "coopLxnijCnHuTCoeXqwXMOoBqFZxJnSE00pL4K2jp5dn"
